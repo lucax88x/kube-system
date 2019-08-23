9e79d4fdd47a834fbeb5b1c174b933f09f0294cb
lucatrazzi <luca.trazzi@garaio.com>
2019-08-23 16:23:14 +0200

    wip

--- scripts/vagrant.get-ips.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# wget https://download.virtualbox.org/virtualbox/6.0.8/Oracle_VM_VirtualBox_Extension_Pack-6.0.8.vbox-extpack
-# VBoxManage extpack install ./Oracle_VM_VirtualBox_Extension_Pack-6.0.8.vbox-extpack
-
-for f in $(VBoxManage list runningvms | awk -F\" '{print $2}'); do
-    echo "$f:"
-    VBoxManage guestproperty enumerate "$f" | grep IP
-done
