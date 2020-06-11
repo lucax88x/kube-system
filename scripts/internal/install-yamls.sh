b8f501165af58458ac9eac5f72846d0a2d134697
lucatrazzi <luca.trazzi@garaio.com>
2020-06-11 16:23:51 +0200

    actualizing

--- scripts/internal/install-yamls.sh
+++ scripts/internal/install-yamls.sh
@@ -18,20 +18,19 @@ kubectl apply -f /vagrant/yaml/nfs-client/nfs-client.rbac.yaml
 kubectl apply -f /vagrant/yaml/nfs-client/nfs-client.deployment.yaml
 kubectl apply -f /vagrant/yaml/nfs-client/nfs-client.storage-class.yaml
 
-
 ## install registry
-kubectl apply -f /vagrant/yaml/registry/registry.namespace.yaml 
+kubectl apply -f /vagrant/yaml/registry/registry.namespace.yaml
 
-kubectl apply -f /vagrant/yaml/registry/registry-tls.secret.yaml 
-kubectl apply -f /vagrant/yaml/registry/letsencrypt.job.yaml 
-kubectl apply -f /vagrant/yaml/registry/letsencrypt.service.yaml 
+kubectl apply -f /vagrant/yaml/registry/registry-tls.secret.yaml
+kubectl apply -f /vagrant/yaml/registry/letsencrypt.job.yaml
+kubectl apply -f /vagrant/yaml/registry/letsencrypt.service.yaml
 
-kubectl apply -f /vagrant/yaml/registry/registry.config.yaml 
-kubectl apply -f /vagrant/yaml/registry/registry.secret.yaml 
-kubectl apply -f /vagrant/yaml/registry/registry.persistent-volume.yaml 
-kubectl apply -f /vagrant/yaml/registry/registry.deployment.yaml 
-kubectl apply -f /vagrant/yaml/registry/registry.service.yaml 
-kubectl apply -f /vagrant/yaml/registry/registry.ingress.yaml 
+kubectl apply -f /vagrant/yaml/registry/registry.config.yaml
+kubectl apply -f /vagrant/yaml/registry/registry.secret.yaml
+kubectl apply -f /vagrant/yaml/registry/registry.persistent-volume.yaml
+kubectl apply -f /vagrant/yaml/registry/registry.deployment.yaml
+kubectl apply -f /vagrant/yaml/registry/registry.service.yaml
+kubectl apply -f /vagrant/yaml/registry/registry.ingress.yaml
 
 # create nginx-ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
